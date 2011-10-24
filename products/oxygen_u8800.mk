b54b14afd19b6969b3fe83dcfb99b3c97e143426
Martin Johnson <M.J.Johnson@massey.ac.nz>
2011-10-25 09:44:38 +1300

    Update to 2.3.7

--- products/oxygen_u8800.mk
+++ products/oxygen_u8800.mk
@@ -11,16 +11,15 @@ $(call inherit-product, vendor/oxygen/products/gsm.mk)
 # Setup device specific product configuration.
 #
 PRODUCT_NAME := oxygen_u8800
-PRODUCT_BRAND := huawei
+PRODUCT_BRAND := Huawei
 PRODUCT_DEVICE := u8800
-PRODUCT_MODEL := Huawei u8800
-PRODUCT_MANUFACTURER := Huawei
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u8800 BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=google/passion/passion:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.4 GRJ22 121341 release-keys"
-
-# Broadcom FM radio
-#$(call inherit-product, vendor/oxygen/products/bcm_fm_radio.mk)
+PRODUCT_MODEL := u8800
+PRODUCT_MANUFACTURER := HUAWEI
+PRODUCT_BOARD := U8800
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=U8800 BUILD_ID=GRK39F BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=google/passion/passion:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.managed.roaming=1 \
     ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
-    ro.ril.emc.mode=2
+    ro.ril.emc.mode=2 \
+    ro.modversion=Oxygen-2.3.1-u8800
